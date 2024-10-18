f1ffee7a008ea488d1282e015c1ecf4fed919cb5
Shintaamelia22 <shintaap77@gmail.com>
2024-10-18 18:58:01 +0700

    Ubah gambar dan konten yudisium

--- constant/imageMap.ts
+++ constant/imageMap.ts
@@ -5,8 +5,8 @@ const imageMap: { [key: string]: any } = {
     'beasiswa2': require('assets/beasiswa/2.jpg'),
     'lomba1': require('assets/lomba/1.jpg'),
     'lomba2': require('assets/lomba/2.jpg'),
-    'yudisium1': require('assets/yudisium/1.jpg'),
-    'yudisium2': require('assets/yudisium/2.jpg'),
+    'yudisium1': require('assets/yudisium/1.png'),
+    'yudisium2': require('assets/yudisium/2.jpeg'),
   };
   
   export default imageMap;
