796620b555273909fa03a0257512a84fea00cfc2
chyrhmii <cahya.rhmi@gmail.com>
2024-10-18 18:26:43 +0700

    Ubah gambar dan konten beasiswa

--- constant/imageMap.ts
+++ constant/imageMap.ts
@@ -1,7 +1,7 @@
 const imageMap: { [key: string]: any } = {
     'event1': require('assets/event/1.jpg'),
     'event2': require('assets/event/2.jpg'),
-    'beasiswa1': require('assets/beasiswa/1.jpg'),
+    'beasiswa1': require('assets/beasiswa/1.webp'),
     'beasiswa2': require('assets/beasiswa/2.jpg'),
     'lomba1': require('assets/lomba/1.jpg'),
     'lomba2': require('assets/lomba/2.jpg'),
